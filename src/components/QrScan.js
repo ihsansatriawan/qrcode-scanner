f6025e7b2076adcba5ba7f746997cbdaeaed6216
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-10-20 15:17:58 +0700

    Adding toaster

--- src/components/QrScan.js
+++ src/components/QrScan.js
@@ -5,12 +5,27 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Snackbar from '@material-ui/core/Snackbar';
 import QrReader from "react-qr-reader";
 
 const QrScan = () => {
   const [resultScanner, setResultScanner] = useState("");
   const [resultScannerDate, setResultScannerDate] = useState("");
   const [openDialog, setOpenDialog] = useState(false);
+  const [showProgress, setShowProgress] = useState(false);
+
+  const [openNotif, setOpenNotif] = useState(false);
+  const [messageInfo, setMessageInfo] = useState("");
+
+  const handleOpenNotif = (message) => {
+    setOpenNotif(true)
+    setMessageInfo(message)
+  };
+
+  const handleCloseNotif = () => {
+    setOpenNotif(false)
+  };
 
   const handleClickOpen = () => {
     setOpenDialog(true);
@@ -19,6 +34,14 @@ const QrScan = () => {
   const handleClose = () => {
     setOpenDialog(false);
   };
+
+  const handleShowProgress = () => {
+    setShowProgress(true)
+  }
+
+  const handleCloseProgress = () => {
+    setShowProgress(false)
+  }
 
   const handleError = (err) => {
     console.error(err);
@@ -35,18 +58,57 @@ const QrScan = () => {
   const handleSubmit = () => {
     console.log("resultScanner: ", resultScanner)
     console.log("resultScannerDate: ", resultScannerDate)
-    //TODO: Do Submit data to Google Sheet here
-
+    handleShowProgress()
+    // const url = "https://script.google.com/macros/s/AKfycbyBIdsfOcqLtB-MoL8BvE5d65q43nrJzYk-boK2Iw/exec?action=insert&tableName=KKP&name=ihsan%20Insom&waktuDatang=Selasa"
+    const url = "https://script.google.com/macros/s/AKfycbyBIdsfOcqLtB-MoL8BvE5d65q43nrJzYk-boK2Iw/exec?action=insert"
+    const tableNme = "KKP";
+    const name = resultScanner
+    const waktuDatang = resultScannerDate
+    const finalUrl = encodeURI(`${url}&tableName=${tableNme}&name=${name}&waktuDatang=${waktuDatang}`)
+    
+    fetch(finalUrl, {
+      method: 'POST',
+      mode: 'cors',
+      dataType: 'jsonp'
+    })
+    .then(resp => {
+      console.log("resp: ", resp)
+      handleCloseProgress()
+      handleOpenNotif("Sukses Menyimpan Data")
+    })
+    .catch(err => {
+      console.log("error: ", err)
+      handleCloseProgress()
+      handleOpenNotif("GAGAL!")
+    })
+    //TODO: Do Loader Disable
     handleClose();
   }
 
   return <Paper style={{ margin: 16, padding: 16 }}>
-    <QrReader
-      delay={500}
-      onError={handleError}
-      onScan={handleScan}
-      style={{ width: "100%" }}
+    <Snackbar
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      key={'top,center'}
+      open={openNotif}
+      onClose={handleCloseNotif}
+      ContentProps={{
+        'aria-describedby': 'message-id',
+      }}
+      message={messageInfo}
     />
+    {showProgress ? 
+      <>
+        <CircularProgress />
+        <h1>Loading....</h1>
+      </>
+     :
+      <QrReader
+        delay={500}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: "100%" }}
+      />
+    }
     <Dialog open={openDialog} onClose={handleClose} aria-labelledby="form-dialog-title">
       <DialogTitle id="form-dialog-title">Yakin Simpan Data ?</DialogTitle>
       <DialogContent>
